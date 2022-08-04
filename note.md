c12fa7e115cd75b6e24b709c647d6d67de0ea76a
ccrsxx <aminrisal@gmail.com>
2022-08-04 20:54:41 +0700

    add things up

---
+++ note.md
@@ -0,0 +1,7 @@
+# Learn Next.js
+
+Important note about the tutorial:
+
+- Link component also needs an a tag for semantic HTML and SEO.
+- When you want to add an attribute to the Link, add it to the a tag inside.
+- When you need to link to an external page outside the Next.js app, just use an \<a> tag without Link.
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#181A1B',
+        primary: '#E8E6E3',
+        secondary: '#A8A095',
+        link: '#3AA4FF'
+      }
+    }
+  },
+  plugins: []
+};
